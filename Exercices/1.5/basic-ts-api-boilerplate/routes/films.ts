7c245a6b943de1f0755a6537526c2151c23d2690
Louise <louise.li@student.vinci.be>
2024-09-30 15:01:29 +0200

    New ex:1.5

---
+++ Exercices/1.5/basic-ts-api-boilerplate/routes/films.ts
@@ -0,0 +1,78 @@
+import express from "express";
+import { Film, NewFilm } from "../types";
+
+
+const router = express.Router();
+const films: Film[] = [
+    {id : 1,
+    title : "1111aa",
+    director : "aa",
+    duration :100,
+    },
+
+    {id : 2,
+      title : "2222aa",
+      director : "bb",
+      duration :10,
+    },
+
+    {id : 3,
+        title : "3333aa",
+        director : "cc",
+        duration :100,
+        budget: 123
+    }
+];
+
+router.get("/", (req, res) => {
+  if(!req.query["minimum-duration"]){
+    return res.json(films);
+  }
+  const minDuration = Number(req.query["minimum-duration"]);
+  const filteredfilm = films.filter((film) => {
+    return film.duration >= minDuration;
+  });
+  return res.json(filteredfilm);
+});
+
+router.get("/:id",(req, res)=>{
+const id = Number(req.params.id);
+const film = films.find((film) => film.id ===id);
+if (!film) {
+  return res.sendStatus(404);
+}
+return res.json(film);
+});
+
+router.post("/",(req,res)=>{
+  const body: unknown = req.body;
+  if (
+    !body ||
+    typeof body !== "object" ||
+    !("title" in body) ||
+    !("director" in body) ||
+    !("duration" in body) ||
+    typeof body.title !== "string" ||
+    typeof body.director !== "string" ||
+    typeof body.duration !== "number" ||
+    !body.title.trim() ||
+    !body.director.trim() ||
+    body.duration <= 0
+  ){
+    return res.sendStatus(400);
+  }    
+  const {title, director, duration}= body as NewFilm;
+  const nextId =
+    films.reduce((maxId, film) => (film.id > maxId ? film.id : maxId), 0) + 1;
+  const newFilm: Film = {
+    id: nextId,
+    title,
+    director,
+    duration
+  };
+
+  films.push(newFilm);
+  return res.json(newFilm);
+
+});
+export default router;
